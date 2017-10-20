2ae6a54ca29ac021f7d50f66a3c1e3db85801f3f
Chandra Challa <iamcquenx@gmail.com>
2017-10-21 01:41:18 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+app.get('/about', function (req, res) {
+	res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+//console.log(__dirname);
+
+app.listen(PORT, function() {
+	console.log('Express server started on' + PORT + '!')
+});
