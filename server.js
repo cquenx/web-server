56b4a18d3737cdfb01cb6ef6a51ede57830a44a3
Chandra Challa <iamcquenx@gmail.com>
2017-10-21 12:14:59 +0530

    middleware added

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-app.get('/about', function (req, res) {
+var middleware = require('./middleware')
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication ,function (req, res) {
 	res.send('About us!');
 });
 
